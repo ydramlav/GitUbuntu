560c9d09ec6df4d16d669e238ea419fe9c0ed96f
ydramlav <rameshkumar.yedla1378@gmail.com>
2016-12-14 21:54:25 +0530

    only code

--- dev_Ngca/vosp-common/src/main/java/com/bt/vosp/common/service/ProductFeedService.java
+++
@@ -1,8 +0,0 @@
-package com.bt.vosp.common.service;
-
-import com.bt.vosp.common.model.ProductFeedRequestObject;
-import com.bt.vosp.common.model.ProductFeedResponseObject;
-
-public interface ProductFeedService {
-	public ProductFeedResponseObject getProductFeed(ProductFeedRequestObject productFeedRequestObject,String servieType);
-}
--- dev_Ngca/vosp-dataaccessadapter/src/main/java/com/bt/vosp/daa/mpx/keyservice/impl/processor/UserKey.java
+++
@@ -1,9 +0,0 @@
-package com.bt.vosp.daa.mpx.keyservice.impl.processor;
-
-import java.io.StringWriter;
-
-public class UserKey {
-	StringWriter s = new StringWriter();
-
-	
-}
